1c3ba28d1a51f9371e376e7f70c4af607fcbef0b
Luiz <lvj253@gmail.com>
2021-04-17 14:46:54 -0300

    chat a cada 3 segundos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const STATUS_CODE_OK = 200;
 const STATUS_CODE_BADREQUEST = 400;
 let myUserName;
-let keepLoggedInterval;
+let keepLoggedInterval = null;
+let keepChatInterval = null;
 
 function loginUser(){
     myUserName = document.querySelector(".input-username").value;
@@ -28,6 +29,43 @@ function keepLogged(){
 function allowChat(){
     let loginPage = document.querySelector(".login-page");
     loginPage.classList.add("logged");
+    loadChat();
+}
+
+function loadChat(){
+    const chatPromise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages");
+    
+    chatPromise.then((response)=>{
+        keepChatInterval = setInterval(()=>{
+            console.log(response.data);
+            loadMessages(response.data);
+            keepChat();
+        }, 3000);
+    });
+    chatPromise.catch(failLoadInitialChat);
+}
+
+function keepChat(){
+    const chatPromise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages");
+    chatPromise.then((response)=>{
+        loadMessages(response.data);
+    });
+    chatPromise.catch(failKeepChat);
+}
+function loadMessages(vectorMessages){
+    /*AQUIIIIIIIIII */
+    /*AQUIIIIIIIIII */
+    /*AQUIIIIIIIIII */
+    /*AQUIIIIIIIIII */
+    /*AQUIIIIIIIIII */
+}
+function failLoadInitialChat(){
+    alert("Não foi possível carregar suas mensagens\nTente logar-se novamente");
+    restartHomePage();
+}
+function failKeepChat(){
+    alert("Não foi possível recuperar suas mensagens\nTente logar-se novamente");
+    restartHomePage();
 }
 
 function loadingPage(){
@@ -59,7 +97,13 @@ function failKeepLogin(){
 }
 
 function restartHomePage(){
-    clearInterval(keepLoggedInterval);
+    if(keepLoggedInterval){
+        clearInterval(keepLoggedInterval);
+    }
+    
+    if(keepChatInterval){
+        clearInterval(keepChatInterval);
+    }
     let loginPage = document.querySelector(".login-page");
     if (loginPage.classList.contains("logged")){
         loginPage.classList.remove("logged");
@@ -84,97 +128,3 @@ function restartHomePage(){
     loadingImg.classList.add("hide");
     loadingTxt.classList.add("hide");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function loginUser(){
-    const userName = document.querySelector(".input-username");
-    myUserName = userName.value;
-    const data = {name: myUserName};
-    const loginPromise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", data);
-    loginPromise.then(()=>{
-        allowChat();
-        setInterval(keepLogged, 5000);
-    });
-    loginPromise.catch(()=>{
-        alert("Não foi possível logar\nTente novamente");
-        restartHomepage();
-    });
-
-    userName.classList.add("hide");
-    const buttonLogin = document.querySelector(".button-login");
-    buttonLogin.classList.add("hide");
-    const loadingImg = document.querySelector(".loading-img");
-    const loadingTxt = document.querySelector(".loading-text");
-    if(loadingImg.classList.contains("hide") && loadingTxt.classList.contains("hide")){
-        loadingImg.classList.remove("hide");
-        loadingTxt.classList.remove("hide");
-    }
-}
-
-function allowChat(){
-    const loginPage = document.querySelector(".login-page");
-    loginPage.classList.add("logged");
-}
-function keepLogged(){
-    const data = {name: myUserName};
-    console.log(data);
-    const loginPromise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", data);
-    loginPromise.then(()=>{
-        alert("Logou!");
-    });
-    loginPromise.catch((err)=>{
-        logoutUser();
-    });
-}
-function logoutUser(){
-    alert("Você foi deslogado '-'\nLogue-se novamente")
-    const loginPage = document.querySelector(".login-page");
-    if(loginPage.classList.constains("logged")){
-        loginPage.classList.remove("logged");
-    }
-    restartHomepage();
-}
-function restartHomepage(){
-    const userName = document.querySelector(".input-username");
-    if(userName.classList.constains("hide")){
-        userName.classList.remove("hide");
-    }
-    const buttonLogin = document.querySelector(".button-login");
-    if(buttonLogin.classList.constains("hide")){
-        buttonLogin.classList.remove("hide");
-    }
-    const loadingImg = document.querySelector(".loading-img");
-    const loadingTxt = document.querySelector(".loading-text");
-    loadingImg.classList.add("hide");
-    loadingTxt.classList.add("hide");
-}*/
